5f8446926f246d8fd97cfa7ca94c6a269499260e
victor95pc <victorpalomocastro@gmail.com>
2016-02-11 08:51:06 -0200

    Definir layout padrão para a caixa sindical

--- lib/brcobranca/boleto/caixa_sindical.rb
+++ lib/brcobranca/boleto/caixa_sindical.rb
@@ -1,18 +1,12 @@
 module Brcobranca
 	module Boleto
 		class CaixaSindical < Caixa
+			define_template(:rghost_sindical_empresa).each do |klass|
+				extend klass
+				include klass
+			end
+			
 			# Aviso essas regras estão fixas para para sindical !!!
-
-			# Monta a segunda parte do código de barras.
-			#  1 à 6: código do cedente, também conhecido como convênio
-			#  7: dígito verificador do código do cedente
-			#  8 à 10: dígito 3 à 5 do nosso número
-			#  11: dígito 1 do nosso número (modalidade da cobrança)
-			#  12 à 14: dígito 6 à 8 do nosso número
-			#  15: dígito 2 do nosso número (emissão do boleto)
-			#  16 à 24: dígito 9 à 17 do nosso número
-			#  25: dígito verificador do campo livre
-			# @return [String]
 			def codigo_barras_segunda_parte
 				campo_livre = '97' \
 				"#{documento_cedente_sicas[8, 5]}" \
@@ -26,11 +20,7 @@ module Brcobranca
 
 			# Forçar logo da caixa
 			def logotipo
-				if Brcobranca.configuration.gerador == :rghost_carne
-					File.join(File.dirname(__FILE__), '..', 'arquivos', 'logos', 'caixa_carne.eps')
-				else
-					File.join(File.dirname(__FILE__), '..', 'arquivos', 'logos', 'caixa.eps')
-				end
+				File.join(File.dirname(__FILE__), '..', 'arquivos', 'logos', 'caixa.eps')
 			end
 		end
 	end
