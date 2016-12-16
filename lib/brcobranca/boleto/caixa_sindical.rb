a351b9f09d473ec73069948f307f1c9483e0135d
Iuri Madeira <iurimadeira@gmail.com>
2016-12-16 09:27:13 -0200

    Altera geração da segunda parte do codigo de barras para se adaptar ao novo padrão

--- lib/brcobranca/boleto/caixa_sindical.rb
+++ lib/brcobranca/boleto/caixa_sindical.rb
@@ -5,17 +5,30 @@ module Brcobranca
 				extend klass
 				include klass
 			end
-			
+
 			# Aviso essas regras estão fixas para para sindical !!!
 			def codigo_barras_segunda_parte
-				campo_livre = '97' \
-				"#{documento_cedente_sicas[8, 5]}" \
-				'7' \
-				'1' \
-				'77' \
-				"#{numero_documento[3, 12]}" \
-				'42'
-				"#{campo_livre}"
+				codigo_beneficiario = "775800"
+				dv_beneficiario 		= codigo_beneficiario.modulo11(
+					multiplicador: (2..9).to_a,
+					mapeamento: { 10 => 0, 11 => 0 }
+				) { |t| 11 - (t % 11) }
+				sequencia_1					= numero_documento[0, 3]
+				constante_1 				= 1 # Tipo de cobranca (1 - Registrada)
+				sequencia_2					= numero_documento[3, 3]
+				constante_2 				= 4 # Identificador de Emissao do Boleto (4-Beneficiario)
+				sequencia_3					= numero_documento[6, 9]
+
+				campo_livre = "#{codigo_beneficiario}#{dv_beneficiario}" \
+											"#{sequencia_1}#{constante_1}" \
+											"#{sequencia_2}#{constante_2}#{sequencia_3}"
+
+				dv_campo_livre = campo_livre.modulo11(
+					multiplicador: (2..9).to_a,
+					mapeamento: { 10 => 0, 11 => 0 }
+				) { |t| 11 - (t % 11) }
+
+				"#{campo_livre}#{dv_campo_livre}"
 			end
 
 			# Forçar logo da caixa
